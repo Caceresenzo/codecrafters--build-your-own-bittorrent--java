2133c6b43cd17baff8275be65e0f80b30246d708
Caceresenzo <caceresenzo1502@gmail.com>
2023-12-24 19:52:43 +0100

    pass 6th stage

--- src/main/java/bittorrent/Main.java
+++ src/main/java/bittorrent/Main.java
@@ -1,12 +1,18 @@
 package bittorrent;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HexFormat;
 import java.util.Map;
 
 import com.google.gson.Gson;
 
 import bittorrent.bencode.Deserializer;
+import bittorrent.bencode.Serializer;
 import bittorrent.torrent.Torrent;
 
 public class Main {
@@ -14,6 +20,7 @@ public class Main {
 	private static final Gson gson = new Gson();
 	//	private static final Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		String command = args[0];
 		if ("decode".equals(command)) {
@@ -33,9 +40,21 @@ public class Main {
 
 			System.out.println("Tracker URL: %s".formatted(torrent.announce()));
 			System.out.println("Length: %d".formatted(torrent.info().length()));
+			System.out.println("Info Hash: %s".formatted(shaInfo(decoded)));
 		} else {
 			System.out.println("Unknown command: " + command);
 		}
+	}
+
+	private static String shaInfo(final Object decoded) throws IOException, NoSuchAlgorithmException {
+		@SuppressWarnings("unchecked")
+		final var info = ((Map<String, Object>) decoded).get("info");
+
+		final var infoOutputStream = new ByteArrayOutputStream();
+		new Serializer().write(info, infoOutputStream);
+
+		final var digest = MessageDigest.getInstance("SHA-1").digest(infoOutputStream.toByteArray());
+		return HexFormat.of().formatHex(digest);
 	}
 
 }
---
+++ src/main/java/bittorrent/bencode/Serializer.java
@@ -0,0 +1,67 @@
+package bittorrent.bencode;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+public class Serializer {
+
+	public static final byte INTEGER_BYTE = 'i';
+	public static final byte COLON_BYTE = ':';
+	public static final byte END_BYTE = 'e';
+	public static final byte LIST_BYTE = 'l';
+	public static final byte MAP_BYTE = 'd';
+
+	public void write(Object root, OutputStream outputStream) throws IOException {
+		if (root instanceof String string) {
+			writeString(string, outputStream);
+		} else if (root instanceof Long number) {
+			writeNumber(number, outputStream);
+		} else if (root instanceof List<?> list) {
+			writeList(list, outputStream);
+		} else if (root instanceof Map<?, ?> map) {
+			writeMap(map, outputStream);
+		} else {
+			throw new UnsupportedOperationException("unsupported type: " + root.getClass());
+		}
+	}
+
+	public void writeString(String string, OutputStream outputStream) throws IOException {
+		outputStream.write(String.valueOf(string.length()).getBytes());
+		outputStream.write(COLON_BYTE);
+		outputStream.write(string.getBytes(StandardCharsets.ISO_8859_1));
+	}
+
+	public void writeNumber(long number, OutputStream outputStream) throws IOException {
+		outputStream.write(INTEGER_BYTE);
+		outputStream.write(String.valueOf(number).getBytes());
+		outputStream.write(END_BYTE);
+	}
+
+	public void writeList(List<?> list, OutputStream outputStream) throws IOException {
+		outputStream.write(LIST_BYTE);
+
+		for (final var element : list) {
+			write(element, outputStream);
+		}
+
+		outputStream.write(END_BYTE);
+	}
+
+	public void writeMap(Map<?, ?> map, OutputStream outputStream) throws IOException {
+		outputStream.write(MAP_BYTE);
+
+		for (final var entry : map.entrySet()) {
+			final var key = entry.getKey();
+			final var value = entry.getValue();
+
+			writeString((String) key, outputStream);
+			write(value, outputStream);
+		}
+
+		outputStream.write(END_BYTE);
+	}
+
+}
