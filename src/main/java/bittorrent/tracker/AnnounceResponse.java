6d2ad2f9c18b0a4bb98a71c12050a376269f4b16
Caceresenzo <caceresenzo1502@gmail.com>
2023-12-25 15:43:46 +0100

    fix: swap high and low port byte

--- src/main/java/bittorrent/tracker/AnnounceResponse.java
+++ src/main/java/bittorrent/tracker/AnnounceResponse.java
@@ -24,7 +24,7 @@ public record AnnounceResponse(
 
 		for (var start = 0; start < peersString.length; start += 6) {
 			final var address = Arrays.copyOfRange(peersString, start, start + 4);
-			final var port = ((peersString[start + 5] & 0xff) << 8) + (peersString[start + 4] & 0xff);
+			final var port = ((peersString[start + 4] & 0xff) << 8) + (peersString[start + 5] & 0xff);
 
 			final var peer = new InetSocketAddress(InetAddress.getByAddress(address), port);
 			peers.add(peer);
