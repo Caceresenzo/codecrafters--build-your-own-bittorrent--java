61c85060cc14bcac30d7e200ca311c3e2b720006
Caceresenzo <caceresenzo1502@gmail.com>
2024-10-19 18:31:20 +0200

    refactor: messages

---
+++ src/main/java/bittorrent/bencode/BEncoded.java
@@ -0,0 +1,5 @@
+package bittorrent.bencode;
+
+public class BEncoded<T> {
+
+}
---
+++ src/main/java/bittorrent/peer/protocol/Message.java
@@ -0,0 +1,67 @@
+package bittorrent.peer.protocol;
+
+import java.util.Map;
+
+import bittorrent.bencode.BEncoded;
+
+public sealed interface Message {
+
+	public record KeepAlive() implements Message {}
+
+	public record Choke() implements Message {}
+
+	public record Unchoke() implements Message {}
+
+	public record Interested() implements Message {}
+
+	public record NotInterested() implements Message {}
+
+	public record Have(
+		int pieceIndex
+	) implements Message {}
+
+	public record Bitfield(
+		byte[] values
+	) implements Message {
+
+		@Override
+		public final String toString() {
+			return "Bitfield[values.length=%d]".formatted(values.length);
+		}
+
+	}
+
+	public record Request(
+		int index,
+		int begin,
+		int length
+	) implements Message {}
+
+	public record Piece(
+		int index,
+		int begin,
+		byte[] block
+	) implements Message {
+
+		@Override
+		public final String toString() {
+			return "Piece[index=%d, begin=%d, block.length=%d]".formatted(index, begin, block.length);
+		}
+
+	}
+
+	public record Cancel(
+		int index,
+		int begin,
+		int length
+	) implements Message {}
+
+	public record Port(
+		short port
+	) implements Message {}
+
+	public record Extension(
+		BEncoded<Map<String, Object>> body
+	) implements Message {}
+
+}
